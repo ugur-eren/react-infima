7d89f936b5849e8aef777f69856fff4f087533ec
Uğur Eren <contact@ugureren.net>
2022-02-12 15:45:21 +0300

    Badge component

---
+++ src/components/Badge/Badge.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import classNames from 'classnames';
+import {Theme} from '../../Types';
+import {DefaultTheme} from '../../Constants';
+
+interface Props extends React.HTMLAttributes<HTMLSpanElement> {
+  className?: string;
+  theme?: Theme;
+}
+
+const Badge: React.FC<Props> = (props) => {
+  const {children, className, theme = DefaultTheme, ...restProps} = props;
+
+  const classes = classNames(
+    'badge',
+
+    {'badge--primary': theme === 'primary'},
+    {'badge--secondary': theme === 'secondary'},
+    {'badge--success': theme === 'success'},
+    {'badge--info': theme === 'info'},
+    {'badge--warning': theme === 'warning'},
+    {'badge--danger': theme === 'danger'},
+
+    className,
+  );
+
+  return (
+    <span className={classes} {...restProps}>
+      {children}
+    </span>
+  );
+};
+
+export default Badge;
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
 export {default as Button} from './Button/Button';
 export {default as Alert} from './Alert/Alert';
+export {default as Badge} from './Badge/Badge';
