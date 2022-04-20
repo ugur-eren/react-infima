faf738e5e4051e02bb3b3edbb4ebf6f673dd91ee
Uğur Eren <contact@ugureren.net>
2022-04-20 13:57:43 +0300

    create array of themes

--- src/Constants.ts
+++ src/Constants.ts
@@ -1,5 +1,7 @@
 import {Size, Theme} from './Types';
 
+export const Themes: Theme[] = ['primary', 'secondary', 'success', 'info', 'warning', 'danger'];
+
 export const DefaultTheme: Theme = 'secondary';
 
 export const DefaultSize: Size = 'normal';
